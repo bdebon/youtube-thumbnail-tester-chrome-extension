1dafaa1ca3178eb3a8607288769c669999877ee3
David-49 <dd08.brad@hotmail.fr>
2021-03-28 00:08:16 +0100

    disable actions on extension if not youtube

--- background.js
+++ background.js
@@ -3,4 +3,31 @@ let color = '#3aa757';
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+
+
+  //I wanted to factorize the code with a named function but it doesn't seem to work with the system of extension
+  //Event listener when we go to another tab (already open)
+  chrome.tabs.onActivated.addListener(async (info) => {
+    const tab = await chrome.tabs.get(info.tabId);
+    const isYoutube = tab.url.startsWith("https://www.youtube.com/");
+    console.log(tab.url);
+    console.log(isYoutube);
+    
+    //Disable actions on the extension if the url retrieved is not https://www.youtube.com/
+    isYoutube
+      ? chrome.action.enable(tab.tabId)
+      : chrome.action.disable(tab.tabId);
+  });
+
+  //Event listener when a tab is updated
+  chrome.tabs.onUpdated.addListener(async (info) => {
+    const tab = await chrome.tabs.get(info.tabId);
+    const isYoutube = tab.url.startsWith("https://www.youtube.com/");
+    console.log(tab.url);
+    console.log(isYoutube);
+    
+    isYoutube
+      ? chrome.action.enable(tab.tabId)
+      : chrome.action.disable(tab.tabId);
+  });
 });
